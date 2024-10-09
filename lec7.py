b39cae0bc01c3276a6593188581a71387f05bb1c
Prainex <51273142+Prainex@users.noreply.github.com>
2024-10-09 14:09:21 -0400

    Codes up until now

---
+++ lec7.py
@@ -0,0 +1,20 @@
+# for i in range(1001):
+#     cur = i
+#     sumDigits = 0
+
+#     while cur > 0: 
+#         sumDigits = cur%10
+#         cur //=10 
+#         if sumDigits == 20:
+#             print (i)
+
+# # WRite a program using for loops to print the following:
+
+# for i in range(1, 5):
+#     for j in range(1,5):
+#         print ("(", i, ",", j, ")")
+
+# # Write a program using for loops to print the following:
+# for i in range(1,5):
+#     for j in range(1,5):
+#         print (f'({i},{j})')
