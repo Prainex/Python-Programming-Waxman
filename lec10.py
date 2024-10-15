155f7e94d17d9ed88c2a82474d63db4ec7973d13
Prainex <51273142+Prainex@users.noreply.github.com>
2024-10-15 13:51:55 -0400

    weewoo

---
+++ lec10.py
@@ -0,0 +1,15 @@
+# def add (x, y):
+#     return x + y
+# a = 1
+# print (add(a, a+1))
+
+
+"""def f(x, items=[]):
+    items.append(x)
+    return items
+
+print(f(1))
+print(f(2))
+print(f(3))
+"""
+
