42a835c5831dfe4709f5ee2aaf6e44bcd119d24b
Prainex <51273142+Prainex@users.noreply.github.com>
2024-11-03 14:26:51 -0500

    Nov 3rd

---
+++ lec14.py
@@ -0,0 +1,9 @@
+'''
+a = [34, 80, 7, 1, 20, 30, 1, 504, 28]
+print (a)
+#Given a list, print the elements of that list in reverse order. Do this in two ways.
+
+a.sort(reverse=True)
+print (a)
+'''
+
